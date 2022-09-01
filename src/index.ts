3daf26e28ee0167e9e9d1ae2712178afcaae0835
vlad-boroda <enity.w@gmail.com>
2022-09-01 23:16:44 +0300

    fix index file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,2 @@
-import { Migrate } from './migrate';
-
-const main = () => {
-  const migrate = new Migrate();
-
-  const res = migrate.up({
-    source: 'file://migrations',
-    database: 'mysql://root:123321a@tcp(10.43.184.163:3306)/test_db',
-  });
-
-  console.log(res);
-};
-
-main();
+export * from './migrate';
+export * from './types';
